549fb2e6cd8a5365b63d79f7fea5d03f784824c0
Fengwei Yu <fengwei.yu@lewagon.com>
2022-06-03 14:39:20 +0200

    Adding /api/v1/products endpoint

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+PRODUCTS = {
+    1: { 'id': 1, 'name': 'Skello' },
+    2: { 'id': 2, 'name': 'Socialive.tv' },
+}
+
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/api/v1/products')
+def api():
+    return PRODUCTS
